cae4cd9d40dbcd13e757695057d30aa924e6d0e2
Nils Jarle Haugen <nils@gaupne.net>
2020-08-27 11:55:11 +0200

    moved media object picture to domain folder

--- mobapp1/src/main/java/no/nilsjh/ntnu/mobapp1/domain/Picture.java
+++ mobapp1/src/main/java/no/nilsjh/ntnu/mobapp1/domain/Picture.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package no.nilsjh.ntnu.mobapp1.resources;
+package no.nilsjh.ntnu.mobapp1.domain;
 
 /**
  *
