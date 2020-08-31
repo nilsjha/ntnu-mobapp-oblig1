e92484b456fbef82f0c6a26755c2be7616551130
Nils Jarle Haugen <nils+git@gaupne.net>
2020-08-31 21:46:30 +0200

    updated config

---
+++ mobapp4/src/main/java/no/nilsjarh/ntnu/mobapp4/resources/Person.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package no.nilsjarh.ntnu.mobapp4.resources;
+
+/**
+ *
+ * @author nils
+ */
+public class Person {
+    private int id;
+    private String email;
+    private String password;
+    private String address;
+    private String mobilePhone;
+    private String firstName;
+    private String lastName;
+}
