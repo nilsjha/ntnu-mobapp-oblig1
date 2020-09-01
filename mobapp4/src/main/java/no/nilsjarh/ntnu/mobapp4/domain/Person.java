2bce5cad04ffbcfc8afba2a138f405e4544eb300
Nils Jarle Haugen <nils+git@gaupne.net>
2020-09-01 13:37:06 +0200

    added jax-rs service class

---
+++ mobapp4/src/main/java/no/nilsjarh/ntnu/mobapp4/domain/Person.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package no.nilsjarh.ntnu.mobapp4.domain;
+
+/**
+ *
+ * @author nils
+ */
+public class Person {
+    private int id;
+    private String email;
+    private String password;
+    private String address;
+    private String mobilePhone;
+    private String firstName;
+    private String lastName;
+
+
+    public int getId() {
+	    return this.id;
+    }
+
+    public String getEmail() {
+	    return this.email;
+    }
+
+    public String getPassword() {
+	    return this.password;
+    }
+    public String getAddress() {
+	    return this.address;
+    }
+    public String getMobile() {
+	    return this.mobilePhone;
+    }
+    public String getFirstName() {
+	    return this.firstName;
+    }
+    public String getLastName() {
+	    return this.lastName;
+    }
+    
+    public void setId(int id) {
+	    this.id = id;
+    }
+    
+    public void setEmail(String email) {
+	    this.email = email;
+    }
+    
+    public void setPassword(String password) {
+	    this.password = password;
+    }
+    
+    public void setMobile(String number) {
+	    this.mobilePhone = number;
+    }
+    
+    public void setFirstName(String name) {
+	    this.firstName = name;
+    }
+    
+   public void setLastName(String name) {
+	  this.lastName = name;
+   }
+   
+}
