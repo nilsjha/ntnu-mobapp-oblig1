a4300c316f4a5aa583a558f1ae2f830f25613803
Nils Jarle Haugen <nils+git@gaupne.net>
2020-09-09 13:31:42 +0200

    scaffolding media item

---
+++ mobapp4/src/main/java/no/nilsjarh/ntnu/mobapp4/domain/Attachment.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package no.nilsjarh.ntnu.mobapp4.domain;
+
+import javax.activation.MimeType;
+import javax.json.bind.annotation.JsonbTransient;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author nils
+ */
+@Entity(name = "attachments")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false, exclude={"attachedItem"})
+public class Attachment {
+	
+	@Id
+	@Column(name = "id", nullable = false)
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	Long id;
+	
+	
+	private String description;
+	
+	private MimeType type;
+	
+	
+	private String path;
+
+	/**
+	 * OWNING SIDE *
+	 */
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.DETACH)
+	@JoinColumn(name = "attached_item_id", referencedColumnName = "id",
+		nullable = false)
+	private Item attachedItem;
+	
+}
